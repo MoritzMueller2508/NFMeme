21c1e17f442202254f2695bbeb62323109eef6cc
Lukas Becker <lukas@becker-development.de>
2022-02-12 17:23:07 +0100

    Upload Image and Metadata
    Needs to be updated to support dynamic attributes

--- pages/api/pinata.ts
+++ pages/api/pinata.ts
@@ -1,10 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import pinataSDK from '@pinata/sdk'
-import {readFileSync} from "fs";
-import {TSMap} from "typescript-map";
+import pinataSDK, {PinataClient, PinataPinResponse} from '@pinata/sdk'
+import {readFileSync,writeFileSync, ReadStream, createReadStream} from "fs";
 
-//function getAPICredentials() : TSMap<string,string> {
 function getAPICredentials() : Map<string,string> {
     let rawData = readFileSync('pinata-test.json');
     let credentials = JSON.parse(rawData.toString());
@@ -17,20 +15,51 @@ function getAPICredentials() : Map<string,string> {
     return result;
 }
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    //res.status(200).json({ name: 'John Doe' })
-    let credentials = getAPICredentials();
+function getFileAsStream(path : string) : ReadStream{
+    const stream = createReadStream(path);
+    return stream;
+}
 
-    let pinata = pinataSDK(credentials.get("key"), credentials.get('secret'));
-    pinata.testAuthentication().then((result) => {
-        //handle successful authentication here
-        console.log(result);
-    }).catch((err) => {
-        //handle error here
-        console.log(err);
-    });
-    res.status(200);
+async function pinFile(pinata : PinataClient, path : string) : Promise<PinataPinResponse>{
+    let stream = getFileAsStream(path);
+    let result = await pinata.pinFileToIPFS(stream);
+
+    console.log(result);
+    return result;
+}
+
+function createJson(ipfsHash:string, name:string ="Test Name", description:string = "Description", address:string = "7bszZHtcMXADEnECUmYhjQL4k7FtKQ2A7nTFWvoUuXey") : string {
+    let metadata = {
+        "name": name,
+        "symbol": "NFMeme",
+        "description": description,
+        "image": "ipfs://" + description,
+        "properties": {
+            "creators": [{"address": address, "share": 100}],
+            "files": [{"uri": "ipfs://" + description, "type": "image/png"}]
+        },
+        "collection": {"name": "NFMeme", "family": "memes"}
+    };
+
+    let filename = ipfsHash + '.json';
+    let data = JSON.stringify(metadata);
+
+    writeFileSync(filename, data);
+
+    return filename;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    let credentials = getAPICredentials()
+
+    let pinata = pinataSDK(credentials.get("key") as string, credentials.get('secret') as string)
+    let image = await pinFile(pinata, "HEXNFT/003226.png")
+    let nftJsonFile = createJson(image["IpfsHash"])
+    let metadata = await pinFile(pinata, nftJsonFile)
+    let response = {
+        "image": image,
+        "metadata": metadata
+    }
+
+    res.status(200).json(response)
 }
