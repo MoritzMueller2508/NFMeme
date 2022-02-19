9dbf9104f09e7ee232b5d29e44bf79f936a2f140
MoritzMueller2508 <moritzmueller2508@gmail.com>
2022-02-19 09:46:51 +0100

    added tst-scss, renamed folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar/navbar'
 
 const Home: NextPage = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <Navbar />
+      <h1>Home</h1>
+    </>
   );
 }
 
